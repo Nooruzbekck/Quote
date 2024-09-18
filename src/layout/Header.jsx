aabf498f480a4e4a11312fb3438a3cb8af3a2bf5
Nooruz <abdykalykovnooruz638@gmail.com>
2024-09-18 22:10:05 +0300

    add cursor

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,18 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Header.css";
 
 export const Header = ({ onToggleBlock }) => {
   return (
-    <div>
-      <header>
-        <p className="name-pro">Create Quotes</p>
-        <div className="div">
-          <p onClick={() => onToggleBlock("all-quotes")}>All Quotes</p>
-          <p onClick={() => onToggleBlock("new-quotes")}>New Quotes</p>
-        </div>
-      </header>
-      {/* {count && <h2>hello world</h2>}
-      {count2 && <h1>form</h1>} */}
-    </div>
+    <header>
+      <p className="name-pro">Create Quotes</p>
+      <div className="div">
+        <p onClick={() => onToggleBlock("all-quotes")}>All Quotes</p>
+        <p onClick={() => onToggleBlock("new-quotes")}>New Quotes</p>
+      </div>
+    </header>
   );
 };
