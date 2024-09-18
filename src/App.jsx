ca7aebc4bcfea760674ce45c6cac35b33c134743
unknown <yasinabdulaev800@gmail.com>
2024-09-18 21:04:34 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-function App() {
-  return <div>Quote</div>;
-}
+import React from "react";
+import Button from "./components/Button";
+
+const App = () => {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Button.css";
+
+const Button = () => {
+  return (
+    <div>
+      <button className="button">
+        <a href="#">Sort Ascending</a>
+      </button>
+    </div>
+  );
+};
+
+export default Button;
