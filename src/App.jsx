bde3453620f2cde2045c0f410efe09a6620b3364
Nooruz <abdykalykovnooruz638@gmail.com>
2024-09-20 00:48:05 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-import React from "react";
-
-const App = () => {
-  return <div></div>;
-};
-=======
 function App() {
   return <div></div>;
 }
->>>>>>> a99db3602318bb74c9cf4785e86b7fb33821e385
 
 export default App;
